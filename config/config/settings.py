25933b8cb3145dc15106a44b3c11de51782d0a14
yahyaobeid <yobeid@sfsu.edu>
2025-11-04 23:34:42 -0800

    switched to postgres

--- config/config/settings.py
+++ config/config/settings.py
@@ -84,8 +84,12 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("POSTGRES_DB", "triphelix"),
+        "USER": os.getenv("POSTGRES_USER", "default"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD", "password"),
+        "HOST": os.getenv("POSTGRES_HOST", "localhost"),
+        "PORT": os.getenv("POSTGRES_PORT", "5432"),
     }
 }
 
